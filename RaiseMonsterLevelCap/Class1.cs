34734bd22dd78f82932e1c6300f543f1234f5707
Moffein <22742925+Moffein@users.noreply.github.com>
2021-07-21 10:40:02 -0700

    Added a cap for the level up sound.

--- RaiseMonsterLevelCap/Class1.cs
+++ RaiseMonsterLevelCap/Class1.cs
@@ -6,16 +6,20 @@ using MonoMod.Cil;
 using Mono.Cecil.Cil;
 using BepInEx.Configuration;
 using UnityEngine;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace RaiseMonsterLevelCap
 {
-    [BepInPlugin("com.Moffein.RaiseMonsterLevelCap", "Raise Monster Level Cap", "1.0.3")]
+    [BepInPlugin("com.Moffein.RaiseMonsterLevelCap", "Raise Monster Level Cap", "1.0.4")]
     public class RaiseMonsterLevelCap : BaseUnityPlugin
     {
 		public static float maxLevel;
         public void Awake()
         {
-			maxLevel = (float)base.Config.Bind<uint>(new ConfigDefinition("Settings", "Max Level"), 1000000, new ConfigDescription("Max level that monsters can reach. 94 is vanilla. I have no clue how high it actually can go or what happens if it goes too high, so I just set it to somewhere near the uint limit.")).Value;
+			maxLevel = base.Config.Bind<float>(new ConfigDefinition("Settings", "Max Level"), 1000f, new ConfigDescription("Max level that monsters can reach. 99 is vanilla. Past a certain point in the run, monsters will level up every few seconds.")).Value;
+			float stopSound = base.Config.Bind<float>(new ConfigDefinition("Settings","Max Levelup Sound"), 300f, new ConfigDescription("Max level for the levelup sound to play. This prevents sound spam later in runs")).Value;
+			
 			//Remove level capping when calculating monster level
 			IL.RoR2.Run.RecalculateDifficultyCoefficentInternal += (il) =>
 			{
@@ -27,28 +31,18 @@ namespace RaiseMonsterLevelCap
 				c.Emit<RaiseMonsterLevelCap>(OpCodes.Ldsfld, nameof(RaiseMonsterLevelCap.maxLevel));
 			};
 
-			//Use this to confirm monsters are actually leveling up.
-			/*On.RoR2.CharacterBody.RecalculateStats += (orig, self) =>
+			On.RoR2.LevelUpEffectManager.OnRunAmbientLevelUp += (orig, run) =>
 			{
-				orig(self);
-				if (self.teamComponent.teamIndex != TeamIndex.Player)
+				if (run.ambientLevel <= stopSound)
                 {
-					Debug.Log("HP: " + self.healthComponent.health);
+					orig(run);
                 }
-			};*/
+			};
 		}
     }
 }
 
 namespace R2API.Utils
-{
-	[AttributeUsage(AttributeTargets.Assembly)]
-	public class ManualNetworkRegistrationAttribute : Attribute
-	{
-	}
-}
-
-namespace EnigmaticThunder
 {
 	[AttributeUsage(AttributeTargets.Assembly)]
 	public class ManualNetworkRegistrationAttribute : Attribute
